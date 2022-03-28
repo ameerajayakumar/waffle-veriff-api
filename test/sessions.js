535daef77cc6ac1a12891d8bf675cc25d7fccee2
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-03-28 19:07:01 +0530

    negative tcs

--- test/sessions.js
+++ test/sessions.js
@@ -6,6 +6,8 @@ let token, data;
 const url = '';
 
 describe('Sessions Endpoint', () => {
+  let sessionId;
+
   describe('Positive flows', () => {
     it('GET/ session return 200', async () => {
       token = await createNewSession();
@@ -15,7 +17,7 @@ describe('Sessions Endpoint', () => {
         .set('Authorization', `Bearer ${token}`)
         .then((response) => {
           expect(response.status).to.be.equal(200);
-          expect(response.body).to.be.json();
+          //expect(response.body).to.be.json; --> requires npm install chai-json
           expect(response.body).to.not.be.empty;
           expect(response.body.status).to.be.equal('created');
           expect(response.body.initData.preselectedDocument.type).to.be.equal(
@@ -27,8 +29,39 @@ describe('Sessions Endpoint', () => {
           expect(response.body.initData.language).to.be.equal(data.lang);
           expect(response.body.vendorIntegration).to.not.be.empty;
           expect(response.body).to.have.property('activeVerificationSession');
+          sessionId = response.body.id;
         });
     });
+
+    it('GET/ session with id', async () => {
+      if (sessionId) {
+        await request
+          .get(`?id=${sessionId}`)
+          .set('Authorization', `Bearer ${token}`)
+          .expect(200);
+      } else {
+        throw new Error(`session ID is invalid - ${sessionId}`);
+      }
+    });
+
+    describe('Negative flows', () => {
+      it('GET/ request session without user authorization', async () => {
+        token = await createNewSession();
+        data = await createValidDataSet();
+        return await request.get(url).then((response) => {
+          expect(response.status).to.be.equal(401);
+          expect(response.body.message).to.be.equal('Authentication failed');
+        });
+      });
+
+      it('POST/ session with id: unsupported request', async () => {
+        if (sessionId) {
+          await request
+            .post(`?id=${sessionId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .expect(404);
+        }
+      });
+    });
   });
-  xdescribe('Negative flows', () => {});
 });
